b8ab6afb4443609fc1604280186e0d6e5a064660
Bruno Sousa <brunolpsousa@gmail.com>
2023-02-14 16:12:52 -0300

    style: reorder the autocommands

--- init.lua
+++ init.lua
@@ -102,17 +102,23 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	callback = function()
 		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
 	end,
 })
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "gitcommit", "markdown" },
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
 	callback = function()
-		vim.opt_local.wrap = true
-		vim.opt_local.spell = true
+		vim.cmd("hi link illuminatedWord LspReferenceText")
 	end,
 })
 
@@ -122,12 +128,6 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 		if line_count >= 5000 then
 			vim.cmd("IlluminatePauseBuf")
 		end
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-	callback = function()
-		vim.cmd("hi link illuminatedWord LspReferenceText")
 	end,
 })
 
