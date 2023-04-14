72ce0654bf4292c193775fcc67366a9547419783
Bruno Sousa <brunolpsousa@gmail.com>
2023-04-14 15:42:03 -0300

    feat: add autocmd to remove trailing spaces

--- init.lua
+++ init.lua
@@ -110,6 +110,12 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	end,
 })
 
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	callback = function()
+		vim.cmd([[:%s/\s\+$//e]])
+	end,
+})
+
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	pattern = { "*.java" },
 	callback = function()
