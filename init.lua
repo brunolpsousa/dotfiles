1927f86ff3bfb9ce3d48a362758d3bbb2078c701
Bruno Sousa <brunolpsousa@gmail.com>
2023-09-20 15:57:14 -0300

    add sorter in nvim-tree

--- init.lua
+++ init.lua
@@ -1135,6 +1135,43 @@ if pcall(require, "lazy") then
 			end,
 			opts = {
 				disable_netrw = true,
+				sort = {
+					sorter = function(nodes)
+						local function natural_cmp(left, right)
+							if left.type ~= right.type then
+								return left.type < right.type
+							end
+
+							left = left.name:lower()
+							right = right.name:lower()
+
+							if left == right then
+								return false
+							end
+
+							for i = 1, math.max(string.len(left), string.len(right)), 1 do
+								local l = string.sub(left, i, -1)
+								local r = string.sub(right, i, -1)
+
+								if
+									type(tonumber(string.sub(l, 1, 1))) == "number"
+									and type(tonumber(string.sub(r, 1, 1))) == "number"
+								then
+									local l_number = tonumber(string.match(l, "^[0-9]+"))
+									local r_number = tonumber(string.match(r, "^[0-9]+"))
+
+									if l_number ~= r_number then
+										return l_number < r_number
+									end
+								elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
+									return l < r
+								end
+							end
+						end
+
+						table.sort(nodes, natural_cmp)
+					end,
+				},
 				renderer = {
 					group_empty = true,
 					special_files = {},
