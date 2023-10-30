7df783aa03284f1aa67b9927063ee049ca6393a8
Bruno Sousa <brunolpsousa@gmail.com>
2023-10-30 00:52:14 -0300

    try to improve perf when defining theme

--- zsh/theme.sh
+++ zsh/theme.sh
@@ -2,6 +2,7 @@
 
 alacritty_config="$HOME/.config/alacritty/alacritty.toml"
 tmux_config="$HOME/.config/tmux/tmux.conf"
+isDark=1
 
 get_bg() {
   gdbus call --session                                \
@@ -12,18 +13,20 @@ get_bg() {
   cut -d " " -f2
 }
 
-isDark=$(
-  sleep 0.05
-  isDark=$(get_bg)
-  for _ in {1..5}; do
-    if [[ -n $isDark ]]; then break; fi
-    systemctl --user restart xdg-desktop-portal
-    sleep 0.2
+get_theme() {
+  isDark=$(
+    sleep 0.05
     isDark=$(get_bg)
-  done
-  result=${isDark::1}
-  [[ "$result" == "1" || "$result" != "0" && "$result" != "2" ]] && echo "dark"
-)
+    for _ in {1..5}; do
+      if [[ -n $isDark ]]; then break; fi
+      systemctl --user restart xdg-desktop-portal
+      sleep 0.2
+      isDark=$(get_bg)
+    done
+    result=${isDark::1}
+    [[ "$result" == "1" || "$result" != "0" && "$result" != "2" ]] && echo "dark"
+  )
+}
 
 ch_alacritty() {
   [[ -f "$alacritty_config" ]] && command -vp alacritty >/dev/null || return
@@ -61,8 +64,6 @@ ch_tmux() {
 send_sig_to_editor() {
   if [[ "$EDITOR" =~ 'nvim' ]]; then
     EDITOR='nvim'
-  elif [[ "$EDITOR" =~ 'vim' ]]; then
-    EDITOR='vim'
   fi
 
   pgrep "$EDITOR" >/dev/null || return
@@ -114,7 +115,10 @@ ch_system() {
   fi
 }
 
-ch_system "$@"
-ch_alacritty
-ch_tmux
-send_sig_to_editor
+{
+  get_theme
+  ch_system "$@"
+  ch_alacritty
+  ch_tmux
+  send_sig_to_editor
+} &
