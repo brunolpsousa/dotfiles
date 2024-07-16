af4a7457680df944d0bca6b04aefb433cd47a832
Bruno Sousa <brunolpsousa@gmail.com>
2024-07-15 22:59:21 -0300

    pwsh: add profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,11 @@
+# function Invoke-Starship-PreCommand {
+#   $host.ui.RawUI.WindowTitle = ((Get-Location) | Get-Item).Name
+# }
+# Invoke-Expression (&starship init powershell)
+
+oh-my-posh init pwsh --config C:\Users\bruno\.config\omp.json | Invoke-Expression
+
+$env:GPG_TTY = "$(tty)"
+$env:SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)"
+$env:GNUPGHOME = "C://Users//bruno//.local//share//gnupg"
+gpg-connect-agent updatestartuptty /bye >$null | Invoke-Expression
