566f1d43be02101b74d42f10fb10278dc3d82e49
Bruno Sousa <brunolpsousa@gmail.com>
2024-07-18 22:33:17 -0300

    pwsh: update config

--- profile.ps1
+++ profile.ps1
@@ -6,6 +6,7 @@
 oh-my-posh init pwsh --config C:\Users\bruno\.config\omp.json | Invoke-Expression
 
 $env:GPG_TTY = "$(tty)"
+$env:SSH_AGENT_PID = ""
 $env:SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)"
-$env:GNUPGHOME = "C://Users//bruno//.local//share//gnupg"
-gpg-connect-agent updatestartuptty /bye >$null | Invoke-Expression
+$env:GNUPGHOME = "/C/Users/bruno/.local/share/gnupg"
+gpg-connect-agent updatestartuptty /bye >$null
